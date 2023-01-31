e05f05c3337b087b63b5d6e6f906832e8925bfe3
sajidur rahaman <sajidur78@gmail.com>
2023-01-31 20:32:31 +0600

    Compile xxhash as unmanaged

--- CommonLoader/externals/xxhash.cpp
+++ CommonLoader/externals/xxhash.cpp
@@ -40,5 +40,6 @@
 #define XXH_STATIC_LINKING_ONLY   /* access advanced declarations */
 #define XXH_IMPLEMENTATION   /* access definitions */
 #define XXH_NO_XXH3
+#pragma unmanaged
 
 #include "xxhash.h"
